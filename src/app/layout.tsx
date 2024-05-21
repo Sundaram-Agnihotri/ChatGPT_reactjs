bbb1004988c820461656c6f5c056e4a083a19035
Sundaram Agnihotri <agnihotrisundaram8@gmail.com>
2024-05-21 22:55:34 +0530

    updated codes

---
+++ src/app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "CHATGPT-UWin Bot",
+  description: "I am the CHATGPT Bot. Ask me anything regarding procurement, purchase, and logistics.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
